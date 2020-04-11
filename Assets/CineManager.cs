8c481855880026c62a856f18598aa9b3ac023dc7
ebasile96 <edoardo.basile96@gmail.com>
2020-04-11 18:03:51 +0200

    RECameraLike

---
+++ Assets/CineManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class CineManager : MonoBehaviour
+{
+    public CinemachineBrain Brain;
+    public CinemachineVirtualCamera currentCam;
+
+    void Awake()
+    {
+        Brain = GetComponent<CinemachineBrain>();
+    }
+
+    void Update()
+    {
+        CamDisplay();
+    }
+
+    public void CamDisplay()
+    {
+        Brain.IsLive(currentCam, true);
+        
+    }
+}
---
+++ Assets/VcamBehaviour.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+
+
+public class VcamBehaviour : MonoBehaviour
+{
+    public CinemachineVirtualCamera vcam;
+    public CineManager CineMgr;
+        
+
+    void Awake()
+    {
+        vcam = GetComponentInChildren<CinemachineVirtualCamera>();
+        vcam.gameObject.SetActive(false);
+        CineMgr = FindObjectOfType<CineManager>();
+    }
+
+ public void OnTriggerEnter(Collider other)
+{
+    if(other.gameObject.tag == "Player")
+    {
+        vcam.gameObject.SetActive(true);
+        CineMgr.currentCam = vcam;
+    }
+}
+
+    public void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            vcam.gameObject.SetActive(false);
+        }
+           
+    }
+}
+
+    
